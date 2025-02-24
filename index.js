44110dd153abdd621c6e446c493592e91d8e7b41
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-02-24 22:29:20 +0600

    add get method

--- index.js
+++ index.js
@@ -44,7 +44,9 @@ io.on('connection', (socket) => {
 
 
 
-
+app.get('/', (req, res) => {
+  res.send('Pay Nova Server is running');
+});
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
   })
