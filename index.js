54f6a5ef4e22782bfad8f0fb650c400b0b62a198
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-02-24 22:24:15 +0600

    configer socket io

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const connectDB = require('./config/db');
-
 const dotenv = require('dotenv');
+const http = require('http');
+const { Server } = require('socket.io');
 
-const app = express();
 dotenv.config();
+
+// Initialize Express App
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*', 
+  },
+});
 
 // Connect to MongoDB
 connectDB();
+
 app.use(express.json());
+
+// Handle WebSocket Connection
+io.on('connection', (socket) => {
+  console.log('New client connected:', socket.id);
+
+  socket.on('message', (data) => {
+    console.log('Received:', data);
+
+    // Broadcast to all clients
+    io.emit('message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
+});
+
+
+
+
+
+
+
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
